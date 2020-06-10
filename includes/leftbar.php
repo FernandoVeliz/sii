4063806037f04bca1ff937b4bfd9ed6b62a22f11
FernandoVeliz <39470687+FernandoVeliz@users.noreply.github.com>
2020-06-10 15:34:05 -0500

    first commit

---
+++ includes/leftbar.php
@@ -0,0 +1,61 @@
+<div class="left-sidebar bg-black-300 box-shadow ">
+                        <div class="sidebar-content">
+                            <div class="user-info closed">
+                                <img src="http://placehold.it/90/c2c2c2?text=User" alt="John Doe" class="img-circle profile-img">
+                                <h6 class="title">Admin</h6>
+                            </div>
+                            <!-- /.user-info -->
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">Vista Preliminar</span>
+                                    </li>
+                                    <li>
+                                        <a href="dashboard.php"><i class="fa fa-dashboard"></i> <span>Menú</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">Herramientas</span>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Grupos</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-class.php"><i class="fa fa-bars"></i> <span>Añadir Grupo</span></a></li>
+                                            <li><a href="manage-classes.php"><i class="fa fa fa-server"></i> <span>Administrar Grupos</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+  <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Materias</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-subject.php"><i class="fa fa-bars"></i> <span>Añadir materias</span></a></li>
+                                            <li><a href="manage-subjects.php"><i class="fa fa fa-server"></i> <span>Administrar materias</span></a></li>
+                                           <a href="add-subjectcombination.php"><i class="fa fa-newspaper-o"></i> <span>Asignar materias</span></a></li>
+                                           <a href="manage-subjectcombination.php"><i class="fa fa-newspaper-o"></i> <span>Modificar asignación de materias</span></a></li>
+                                        </ul>
+                                    </li>
+   <li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Alumnos</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-students.php"><i class="fa fa-bars"></i> <span>Añadir alumnos</span></a></li>
+                                            <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>Administrar alumnos</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+<li class="has-children">
+                                        <a href="#"><i class="fa fa-info-circle"></i> <span>Calificaciones</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-result.php"><i class="fa fa-bars"></i> <span>Añadir calificaciones</span></a></li>
+                                            <li><a href="manage-results.php"><i class="fa fa fa-server"></i> <span>Administrar calificaciones</span></a></li>
+                                           
+                                        </ul>
+                                        <li><a href="change-password.php"><i class="fa fa fa-server"></i> <span>Cambiar contraseña</span></a></li>
+                                           
+                                    </li>
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+                    </div>
